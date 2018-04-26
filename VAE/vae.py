ca5c98069179eb91f6cf694ce9491373253391cd
Wei Chieh Hsu <33659173+hyestt@users.noreply.github.com>
2018-04-26 14:04:20 -0500

    Add files via upload

---
+++ VAE/vae.py
@@ -0,0 +1,214 @@
+"""Variation autoencoder."""
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow import contrib
+from tensorflow.contrib import layers
+from tensorflow.contrib.slim import fully_connected
+
+
+class VariationalAutoencoder(object):
+    """Varational Autoencoder.
+    """
+    def __init__(self, ndims=784, nlatent=2):
+        """Initializes a VAE. 
+
+        Args:
+            ndims(int): Number of dimensions in the feature.
+            nlatent(int): Number of dimensions in the latent space.
+        """
+
+        self._ndims = ndims
+        self._nlatent = nlatent
+
+        # Create session
+        self.session = tf.Session()
+        self.x_placeholder = tf.placeholder(tf.float32, [None, ndims])
+        self.learning_rate_placeholder = tf.placeholder(tf.float32, [])
+
+        # Build graph.
+        self.z_mean, self.z_log_var = self._encoder(self.x_placeholder)
+        self.z = self._sample_z(self.z_mean, self.z_log_var)
+        self.outputs_tensor = self._decoder(self.z)
+
+        # Setup loss tensor, predict_tensor, update_op_tensor
+        self.loss_tensor = self.loss(self.outputs_tensor, self.x_placeholder,
+                                     self.z_mean, self.z_log_var)
+
+        self.update_op_tensor = self.update_op(self.loss_tensor,
+                                               self.learning_rate_placeholder)
+
+        # Initialize all variables.
+        self.session.run(tf.global_variables_initializer())
+
+
+
+    def _sample_z(self, z_mean, z_log_var):
+        """Samples z using reparametrization trick.
+
+        Args:
+            z_mean (tf.Tensor): The latent mean,
+                tensor of dimension (None, _nlatent)
+            z_log_var (tf.Tensor): The latent log variance,
+                tensor of dimension (None, _nlatent)
+        Returns:
+            z (tf.Tensor): Random sampled z of dimension (None, _nlatent)
+        """
+
+        eps = tf.random_normal(tf.shape(z_log_var))
+
+        # z = mu + sigma*epsilon
+        #z_log_var stands for log (sigma^2)
+        z = tf.add(z_mean,tf.mul(tf.sqrt(tf.exp(z_log_var)),eps))
+
+        return z
+
+
+    def _encoder(self, x):
+        """Encoder block of the network.
+
+        Builds a two layer network of fully connected layers, with 100 nodes,
+        then 50 nodes, and outputs two branches each with _nlatent nodes
+        representing z_mean and z_log_var.
+
+        Args:
+            x (tf.Tensor): The input tensor of dimension (None, _ndims).
+        Returns:
+            z_mean(tf.Tensor): The latent mean, tensor of dimension
+                (None, _nlatent).
+            z_log_var(tf.Tensor): The latent log variance, tensor of dimension
+                (None, _nlatent).
+        """
+
+      
+        l1 = tf.contrib.layers.fully_connected(x,100,tf.nn.softplus)
+        l2 = tf.contrib.layers.fully_connected(l1,50,tf.nn.softplus)
+        
+        z_mean = tf.contrib.layers.fully_connected(l2,self._nlatent,activation_fn=None)
+        z_log_var = tf.contrib.layers.fully_connected(l2,self._nlatent,activation_fn=None)
+
+
+        return z_mean, z_log_var
+
+    def _decoder(self, z):
+        """From a sampled z, decode back into image.
+
+        Builds a three layer network of fully connected layers,
+        with 50, 100, _ndims nodes.
+
+        z (_nlatent) --> 50 --> 100 --> _ndims.
+
+        Use activation of tf.nn.softplus for hidden layers.
+
+        Args:
+            z(tf.Tensor): z from _sample_z of dimension (None, _nlatent).
+        Returns:
+            f(tf.Tensor): Decoded features, tensor of dimension (None, _ndims).
+        """
+
+        l1 = tf.contrib.layers.fully_connected(z,50,tf.nn.softplus)
+        l2 = tf.contrib.layers.fully_connected(l1,100,tf.nn.softplus)
+        #need to add activation_fn=None so that f can be negative
+        f = tf.contrib.layers.fully_connected(l2,self._ndims,activation_fn=None)
+        
+
+        return f
+
+    def _latent_loss(self, z_mean, z_log_var):
+        """Constructs the latent loss.
+
+        Args:
+            z_mean(tf.Tensor): Tensor of dimension (None, _nlatent)
+            z_log_var(tf.Tensor): Tensor of dimension (None, _nlatent)
+
+        Returns:
+            latent_loss(tf.Tensor): A scalar Tensor of dimension ()
+                containing the latent loss.
+        """
+        latent_loss = None
+
+        D_KL = -0.5*(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+        latent_loss = tf.reduce_mean(D_KL,1)
+
+        return latent_loss
+
+    def _reconstruction_loss(self, f, x_gt):
+        """Constructs the reconstruction loss, assuming Gaussian distribution.
+
+        Args:
+            f(tf.Tensor): Predicted score for each example, dimension (None,
+                _ndims).
+            x_gt(tf.Tensor): Ground truth for each example, dimension (None,
+                _ndims).
+        Returns:
+            recon_loss(tf.Tensor): A scalar Tensor for dimension ()
+                containing the reconstruction loss.
+        """
+        #use l2_loss square
+        recon_loss = tf.reduce_sum((0.5*tf.square((x_gt-f))),1)
+
+
+        return recon_loss
+
+    def loss(self, f, x_gt, z_mean, z_var):
+        """Computes the total loss.
+
+        Computes the sum of latent and reconstruction loss.
+
+        Args:
+            f (tf.Tensor): Decoded image for each example, dimension (None,
+                _ndims).
+            x_gt (tf.Tensor): Ground truth for each example, dimension (None,
+                _ndims)
+            z_mean (tf.Tensor): The latent mean,
+                tensor of dimension (None, _nlatent)
+            z_log_var (tf.Tensor): The latent log variance,
+                tensor of dimension (None, _nlatent)
+
+        Returns:
+            total_loss: Tensor for dimension (). Sum of
+                latent_loss and reconstruction loss.
+        """
+        total_loss = tf.reduce_mean(self._reconstruction_loss(f,x_gt) + self._latent_loss(z_mean,z_var))
+
+
+        return total_loss
+
+    def update_op(self, loss, learning_rate):
+        """Creates the update optimizer.
+
+        Use tf.train.AdamOptimizer to obtain the update op.
+
+        Args:
+            loss(tf.Tensor): Tensor of shape () containing the loss function.
+            learning_rate(tf.Tensor): Tensor of shape (). Learning rate for
+                gradient descent.
+        Returns:
+            train_op(tf.Operation): Update opt tensorflow operation.
+        """
+
+        opt = tf.train.AdamOptimizer(learning_rate)
+        train_op  = opt.minimize(loss)
+
+        return train_op
+
+    def generate_samples(self, z_np):
+        """Generates random samples from the provided z_np.
+
+        Args:
+            z_np(numpy.ndarray): Numpy array of dimension
+                (batch_size, _nlatent).
+
+        Returns:
+            out(numpy.ndarray): The sampled images (numpy.ndarray) of
+                dimension (batch_size, _ndims).
+        """
+
+        out = self.session.run(self.outputs_tensor, feed_dict={self.z: z_np})
+        #if output is larger than 1 -> =1, output smaller than 0 -> =0
+        # becasue of gaussian N(0,1)
+        out[out>1] = 1
+        out[out<0] = 0
+        return out
+
